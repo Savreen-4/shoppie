a08ed3297f0fb3b66bc6ed814db1036c70ecc43b
Savreen-4 <79464605+Savreen-4@users.noreply.github.com>
2021-12-11 20:04:52 +0530

    Add files via upload
    shoppie

---
+++ src/Components/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+
+function Login() {
+    const [user, setUser] = useState([{userName: '', password: ''}]);
+    console.log(user)
+    const history = useHistory();
+
+    const HandleLogin = () => {
+        if(user.userName === 'savreen' && user.password === 'savreen123') {
+            history.push('/product-list');
+        } else {
+            alert('Wrong username or password!');
+        }
+    }
+
+    return (
+        <div>
+            <h3>Login</h3>
+            <label style={{marginRight: 20}}>Username</label>
+            <input type="text" placeholder="Enter username" onChange={(e) => setUser({...user, userName: e.target.value})}></input>
+            <br></br>
+            <br></br>
+            <label style={{marginRight: 20}}>Password</label>
+            <input type="password" placeholder="Enter password" onChange={(e) => setUser({...user, password: e.target.value})}></input>
+            <br></br>
+            <br></br>
+            <button onClick={HandleLogin} disabled={!user.userName || !user.password}>Login</button>
+        </div>
+    )
+}
+
+export default Login
